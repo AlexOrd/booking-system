77ca58a707440b0dd80efc02a8835fdbd2364905
mariiapolova <mariia.polova@eliftech.com>
2018-09-12 11:17:19 +0300

    Added date and time form controls. Created MUI theme (not integrated to component's layout so far)

--- src/components/Root.js
+++ src/components/Root.js
@@ -4,13 +4,20 @@ import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
 import App from './App';
 
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+import theme from './muiTheme';
+
 export default class Root extends Component {
   render() {
     const { store, history } = this.props;
+    const muiTheme = createMuiTheme(theme);
+
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>
+          <MuiThemeProvider theme={muiTheme}>
             <App />
+          </MuiThemeProvider>
         </ConnectedRouter>
       </Provider>
     );
---
+++ src/components/muiTheme.js
@@ -0,0 +1,22 @@
+let muiTheme =  {
+  fontFamily: 'Roboto',
+  palette: {
+    primary1Color: '#1958A8',
+    accent1Color: "#0B314F",
+    secondaryColor: '#EE3361',
+    textColor: 'white',
+    rgb: {
+      accentColor: '25 88 168'
+    }
+  },
+  spacing: {
+    horizontal: 45,
+    vertical: 15
+  },
+  text: {
+    title: '2em',
+    input: '1em'
+  }
+};
+
+export default muiTheme
