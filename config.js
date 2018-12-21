80d03ee8b1f838fee53b69173d1bf77a97364539
Alex Ordynski <Dr.Hikari@gmail.com>
2018-12-21 11:11:35 +0200

    Fix config.

--- config.js
+++ config.js
@@ -1,20 +1,28 @@
+import * as localConfig from './localConfig.js';
+
 const googleApi = process.env.PRODUCTION_MODE == 'ON' ?
   {
     "apiKey": process.env.API_KEY,
     "clientId": process.env.CLIENT_ID,
     "discoveryDocs": [process.env.DISCOVERY_DOCS],
     "scope": process.env.SCOPE
-  } :
-  {
-  };
+  } : localConfig.googleApi;
 
 const openWeatherAPI = process.env.PRODUCTION_MODE == 'ON' ?
   {
     "apiLink" : process.env.WEATHER_API_LINK,
     "apiKey" : process.env.WEATHER_API_KEY,
     "cityCode": process.env.WEATHER_CITY_CODE,
-  } :
-  {
-  };
+  } : localConfig.openWeatherAPI;
 
-export {googleApi, openWeatherAPI};
+const AWSConfigs = process.env.PRODUCTION_MODE == 'ON' ?
+{
+  "AWSAccessKeyId" : process.env.AWS_KEY_ID,
+  "AWSSecretKey" : process.env.AWS_SECRET_KEY,
+  "AWSRegion" : process.env.AWS_REGION,
+  "AWSApiGateway": process.env.AWS_GATEWAY,
+  "ApiGatewayXKey": process.env.AWS_GATEWAY_XKEY,
+  "IoTEndpoint": process.env.AWS_IOT
+} : localConfig.AWSConfigs;
+
+export { googleApi, openWeatherAPI, AWSConfigs };
